839e6f98bf2571bb0a5a9b66f22d8f02341d1af5
armelletamo <armelletamo@yahoo.fr>
2018-02-14 13:40:41 +0100

    reservation controllers

---
+++ WebApplication_GrandHotel/GrandHotel_WebApplication/Controllers/ReservationsController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GrandHotel_WebApplication.Data;
+using GrandHotel_WebApplication.Models;
+
+namespace GrandHotel_WebApplication.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private readonly GrandHotelContext _context;
+
+        public ReservationsController(GrandHotelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reservations
+        public async Task<IActionResult> Index()
+        {
+            var grandHotelContext = _context.Reservation.Include(r => r.IdClientNavigation).Include(r => r.JourNavigation).Include(r => r.NumChambreNavigation);
+            return View(await grandHotelContext.ToListAsync());
+        }
+
+        // GET: Reservations/Details/5
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservation
+                .Include(r => r.IdClientNavigation)
+                .Include(r => r.JourNavigation)
+                .Include(r => r.NumChambreNavigation)
+                .SingleOrDefaultAsync(m => m.NumChambre == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // GET: Reservations/Create
+        public IActionResult VerifDisponibilité(DateTime Jour, int NbNuit, byte NbPersonnes)
+        {
+            bool verif = false;
+
+            if (ModelState.IsValid)
+            {
+                if (_context.Reservation.Where(m => m.Jour == Jour).Include(c=>c.NumChambre).Select(c=>c.NbNuit== !=null||)
+                {
+
+                }
+            }
+            return View();
+        }
+
+        // POST: Reservations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NumChambre,Jour,IdClient,NbPersonnes,HeureArrivee,Travail")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdClient"] = new SelectList(_context.Client, "Id", "Civilite", reservation.IdClient);
+            ViewData["Jour"] = new SelectList(_context.Calendrier, "Jour", "Jour", reservation.Jour);
+            ViewData["NumChambre"] = new SelectList(_context.Chambre, "Numero", "Numero", reservation.NumChambre);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservation.SingleOrDefaultAsync(m => m.NumChambre == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdClient"] = new SelectList(_context.Client, "Id", "Civilite", reservation.IdClient);
+            ViewData["Jour"] = new SelectList(_context.Calendrier, "Jour", "Jour", reservation.Jour);
+            ViewData["NumChambre"] = new SelectList(_context.Chambre, "Numero", "Numero", reservation.NumChambre);
+            return View(reservation);
+        }
+
+        // POST: Reservations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("NumChambre,Jour,IdClient,NbPersonnes,HeureArrivee,Travail")] Reservation reservation)
+        {
+            if (id != reservation.NumChambre)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationExists(reservation.NumChambre))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdClient"] = new SelectList(_context.Client, "Id", "Civilite", reservation.IdClient);
+            ViewData["Jour"] = new SelectList(_context.Calendrier, "Jour", "Jour", reservation.Jour);
+            ViewData["NumChambre"] = new SelectList(_context.Chambre, "Numero", "Numero", reservation.NumChambre);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservation
+                .Include(r => r.IdClientNavigation)
+                .Include(r => r.JourNavigation)
+                .Include(r => r.NumChambreNavigation)
+                .SingleOrDefaultAsync(m => m.NumChambre == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            var reservation = await _context.Reservation.SingleOrDefaultAsync(m => m.NumChambre == id);
+            _context.Reservation.Remove(reservation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservationExists(short id)
+        {
+            return _context.Reservation.Any(e => e.NumChambre == id);
+        }
+    }
+}

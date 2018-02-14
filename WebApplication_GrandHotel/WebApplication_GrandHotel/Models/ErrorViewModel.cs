0fa5176312758550dc2f54678fb1f1cef2c306f1
denis <1>
2018-02-14 09:18:39 +0100

    Demarrage

---
+++ WebApplication_GrandHotel/WebApplication_GrandHotel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication_GrandHotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

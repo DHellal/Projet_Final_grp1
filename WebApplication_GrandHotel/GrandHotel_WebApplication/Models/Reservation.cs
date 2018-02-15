14dcb7b89352ce772dee6041187bb56705882db6
armelletamo <armelletamo@yahoo.fr>
2018-02-15 13:52:34 +0100

    modification de methode qui renvoi la liste des chambres disponible

--- WebApplication_GrandHotel/GrandHotel_WebApplication/Models/Reservation.cs
+++ WebApplication_GrandHotel/GrandHotel_WebApplication/Models/Reservation.cs
@@ -5,24 +5,39 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GrandHotel_WebApplication.Models
 {
-    public partial class Reservation
+    public partial class Reservation : IValidatableObject
     {
         public short NumChambre { get; set; }
         [DataType(DataType.Date)]
         [Display(Name = "Date de début de séjour")]
+        [Required(ErrorMessage = "Saississez la date de début de séjour")]
         public DateTime Jour { get; set; }
         public int IdClient { get; set; }
         [Display(Name = "Nombre de personnes")]
+        [Required(ErrorMessage = "Saississez le nombre de personnes")]
         public byte NbPersonnes { get; set; }
         [Display(Name = "Heure d'arrivée")]
+        [Range(7, 21, ErrorMessage = "entrer un heure entre 7h et 21h")]
         public byte HeureArrivee { get; set; }
         [Display(Name = "Nature du séjour(Travail)")]
         public bool? Travail { get; set; }
         [NotMapped]
         [Display(Name = "Nombre de nuit")]
+        [Required(ErrorMessage = "Saississez le nombre de nuit")]
         public int NbNuit { get; set; }
         public Client IdClientNavigation { get; set; }
         public Calendrier JourNavigation { get; set; }
         public Chambre NumChambreNavigation { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            Reservation reservation = (Reservation)validationContext.ObjectInstance;
+
+            if (reservation.Jour < DateTime.Today)
+            {
+                yield return new ValidationResult("La date saisie est invalide", new string[] { "Jour" });
+            }
+        }
+
     }
 }
